4a3d9bebadc8fdf84e1a86aebd566dcc9547a1fe
Hh440 <harsh100045@gmail.com>
2024-12-11 23:03:34 +0530

    chatRoom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,75 +1,90 @@
 
-import { useEffect, useRef, useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import { ChatRoom } from './pages/ChatRoom'
 
 
 
 function App() {
 
-  const [message,setMessage]= useState<String[]>([])
-  const inputRef=useRef()
-  const wsRef= useRef<WebSocket>()
+  // const [message,setMessage]= useState<String[]>([])
+  // const inputRef=useRef()
+  // const wsRef= useRef<WebSocket>()
 
-  useEffect(()=>{
+  // useEffect(()=>{
 
-    const ws=  new WebSocket("ws://localhost:8080")
-    wsRef.current=ws
+  //   const ws=  new WebSocket("ws://localhost:8080")
+  //   wsRef.current=ws
 
-    ws.onopen=()=>{
-      ws.send((JSON.stringify({
-        type:"join",
-        payload:{
-          roomId:"red"
-        }
-      })))
-    }
-    ws.onmessage=(event)=>{
-      setMessage(m=>[...m,event.data ])
+  //   ws.onopen=()=>{
+  //     ws.send((JSON.stringify({
+  //       type:"join",
+  //       payload:{
+  //         roomId:"red"
+  //       }
+  //     })))
+  //   }
+  //   ws.onmessage=(event)=>{
+  //     setMessage(m=>[...m,event.data ])
 
-    }
+  //   }
 
-  },[])
+  // },[])
 
 
-  const handleClick= ()=>{
-    if(!wsRef.current){
-      return
-    }
+  // const handleClick= ()=>{
+  //   if(!wsRef.current){
+  //     return
+  //   }
 
-    const message= inputRef.current.value;
+  //   const message= inputRef.current.value;
 
-    wsRef.current.send(JSON.stringify({
-      type:"chat",
-      payload:{
-        message:message
-      }
-    }))
+  //   wsRef.current.send(JSON.stringify({
+  //     type:"chat",
+  //     payload:{
+  //       message:message
+  //     }
+  //   }))
 
 
     
-  }
+  // }
 
 
-  return (
-    <div className='h-screen bg-black'>
-      <div className='h-[95vh] '>
-        {message.map(message=>
-        <div className='p-4'>
-        <span className='bg-white text-black p-4 m-8 '>{message}
-          </span>
-          </div>)}
+  // return (
+  //   <div className='h-screen bg-black'>
+  //     <div className='h-[95vh] '>
+  //       {message.map(message=>
+  //       <div className='p-4'>
+  //       <span className='bg-white text-black p-4 m-8 '>{message}
+  //         </span>
+  //         </div>)}
 
-      </div>
-      <div className='w-full  bg-white flex '> 
-        <input ref={inputRef} type='text' className='flex-1 p-4'/>
-        <button  onClick={handleClick} className='bg-purple-700 text=white'>Send Message</button>
+  //     </div>
+  //     <div className='w-full  bg-white flex '> 
+  //       <input ref={inputRef} type='text' className='flex-1 p-4'/>
+  //       <button  onClick={handleClick} className='bg-purple-700 text=white'>Send Message</button>
 
-      </div>
+  //     </div>
 
 
-    </div>
+  //   </div>
   
+  // )
+
+
+
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path='/chatRoom' element={<ChatRoom/>}/>
+        
+
+      </Routes>
+    </BrowserRouter>
   )
+
+
 }
 
 export default App
---
+++ frontend/src/pages/ChatRoom.tsx
@@ -0,0 +1,104 @@
+import { useEffect, useRef, useState } from "react"
+
+
+export const ChatRoom =()=>{
+     
+    const inputRef= useRef<HTMLInputElement>(null)
+
+    const[message,setMessage]=useState<string[]>([])
+
+    const wsRef= useRef<WebSocket>()
+  
+
+    useEffect(()=>{
+        const ws= new WebSocket('ws://localhost:8080')
+
+        wsRef.current=ws
+
+        ws.onopen=()=>{
+            ws.send(JSON.stringify({
+                type:"join",
+                payload:{
+                    roomId:"red"
+                }
+            }))
+        }
+
+
+        ws.onmessage= (event)=>{
+            const newMessage = event.data;
+            setMessage((prevMessages) => {
+                // if (prevMessages.includes(newMessage)) {
+                //     return prevMessages; 
+                // }
+                return [...prevMessages, newMessage];
+            });
+        }
+
+
+        return () => {
+            ws.close();
+        };
+
+
+
+
+
+    },[])
+
+
+    const sendMessage= ()=>{
+
+        if(!wsRef.current){
+                 return
+            }
+
+        if(!inputRef.current){
+            return
+
+        }
+
+        const message= inputRef.current?.value.trim()
+
+        wsRef.current?.send(JSON.stringify({
+            type:"chat",
+            payload:{
+                message:message
+            }
+        }))
+
+        inputRef.current.value=""
+
+
+
+    }
+
+
+
+    return(
+        <div className="h-screen bg-black flex justify-center items-center">
+            <div className="border-2 flex flex-col justify-between w-4/12 h-5/6 rounded-md p-2">
+
+                <div className="border-red-400 overflow-y-auto h-[90vh] no-scrollbar">
+                    {message.map((m,index)=>(
+                        <div key={index} className="flex w-fit pl-2">
+                            <span className="rounded-sm bg-white mb-2 p-2">
+                                {m}
+                            </span>
+                        </div>
+                    ))}
+
+                </div>
+
+                <div className=" flex border-black w-full">
+                    <input ref={inputRef} placeholder="Type your message" className="w-full pl-5" />
+                    <button onClick={sendMessage} className="bg-blue-700">Send Message</button>
+
+                </div>
+
+              
+            </div>
+
+        </div>
+    )
+}
